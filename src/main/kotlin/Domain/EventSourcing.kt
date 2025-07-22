8f21fa478f74c85c1f8214febed0499b43ce22a7
Svaťa Šimara <svatopluk.simara@fairtiq.com>
2025-07-22 14:25:35 +0200

    more code

---
+++ src/main/kotlin/Domain/EventSourcing.kt
@@ -0,0 +1,52 @@
+package com.fairtiq.domain
+
+import java.util.stream.Stream
+import kotlin.streams.asSequence
+
+
+sealed class DomainEvent
+data class GameStarted(val gameId: String, val wordToGuess: String) : DomainEvent()
+data class GuessedCorrectly(val gameId: String, val guess: String) : DomainEvent()
+data class GuessedWrongly(val gameId: String, val guess: String) : DomainEvent()
+
+data class StartNewGameCommand(val gameId: String, val wordToGuess: String) {
+    fun decide(events: Stream<DomainEvent>): Stream<DomainEvent> {
+        guardStarted(events)
+
+        return GameStarted(gameId, wordToGuess).asStream()
+    }
+
+    private fun guardStarted(events: Stream<DomainEvent>) {
+        if (events.anyMatch { it is GameStarted }) {
+            throw IllegalStateException("Game with ID '$gameId' has already been started.")
+        }
+    }
+}
+
+data class GuessWordCommand(val guess: String) {
+    fun decide(events: Stream<DomainEvent>): Stream<DomainEvent> {
+        val state = events.asSequence().fold(State(), ::evolve)
+
+        requireNotNull(state.gameId)
+        requireNotNull(state.wordToGuess)
+
+        return if (state.wordToGuess == guess) {
+            GuessedCorrectly(state.gameId, guess)
+        } else {
+            GuessedWrongly(state.gameId, guess)
+        }.asStream()
+    }
+    data class State(val gameId: String? = null, val wordToGuess: String? = null)
+
+    private fun evolve(state: State, event: DomainEvent): State {
+        return when (event) {
+            is GameStarted -> state.copy(gameId = event.gameId, wordToGuess = event.wordToGuess)
+            else -> state
+        }
+    }
+
+}
+
+private fun <T> T.asStream(): Stream<T> {
+    return Stream.of(this)
+}
--- src/test/kotlin/Learning.kt
+++ src/test/kotlin/Learning.kt
@@ -1,120 +1,146 @@
+import com.fairtiq.domain.DomainEvent
+import com.fairtiq.domain.GameStarted
+import com.fairtiq.domain.GuessWordCommand
+import com.fairtiq.domain.GuessedCorrectly
+import com.fairtiq.domain.GuessedWrongly
+import com.fairtiq.domain.StartNewGameCommand
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import io.cloudevents.CloudEvent
 import io.cloudevents.core.builder.CloudEventBuilder
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Nested
+import org.occurrent.application.converter.jackson.JacksonCloudEventConverter
+import org.occurrent.application.service.blocking.generic.GenericApplicationService
 import org.occurrent.eventstore.api.blocking.EventStream
 import org.occurrent.eventstore.inmemory.InMemoryEventStore
 import java.net.URI
 import java.time.Instant
 import java.time.ZoneOffset
+import java.util.UUID
 import java.util.stream.Stream
+import kotlin.jvm.optionals.getOrNull
 import kotlin.test.Test
-
-
-sealed class DomainEvent
-data class GameStarted(val gameId: String, val wordToGuess: String) : DomainEvent()
-data class GuessedCorrectly(val gameId: String, val guess: String) : DomainEvent()
-data class GuessedWrongly(val gameId: String, val guess: String) : DomainEvent()
-
-data class StartNewGameCommand(val gameId: String, val wordToGuess: String) {
-    fun decide(events: Sequence<DomainEvent>): Sequence<GameStarted> {
-        guardStarted(events)
-
-        return GameStarted(gameId, wordToGuess).asSequence()
-    }
-
-    private fun guardStarted(events: Sequence<DomainEvent>) {
-        if (events.any { it is GameStarted }) {
-            throw IllegalStateException("Game with ID '$gameId' has already been started.")
-        }
-    }
-}
-
-private fun <T> T.asSequence(): Sequence<T> {
-    return sequenceOf(this)
-}
-
-data class GuessWordCommand(val guess: String) {
-    fun decide(events: Sequence<DomainEvent>): Sequence<DomainEvent> {
-        val state = events.fold(State()) { acc, event ->
-            evolve(acc, event)
-        }
-
-        requireNotNull(state.gameId)
-        requireNotNull(state.wordToGuess)
-
-        return if (state.wordToGuess == guess) {
-            GuessedCorrectly(state.gameId, guess)
-        } else {
-            GuessedWrongly(state.gameId, guess)
-        }.asSequence()
-    }
-
-    data class State(val gameId: String? = null, val wordToGuess: String? = null)
-
-    private fun evolve(state: State, event: DomainEvent): State {
-        return when (event) {
-            is GameStarted -> state.copy(gameId = event.gameId, wordToGuess = event.wordToGuess)
-            else -> state
-        }
-    }
-}
 
 class Learning {
 
     @Test
-    fun foo() {
+    fun `what is event`() {
         val event = CloudEventBuilder.v1()
-            .withId("eventId")
-            .withSource(URI.create("urn:mydomain"))
-            .withType("HelloWorld")
+            .withId(UUID.randomUUID().toString())
+            .withSource(URI.create("com:fairtiq:guess_game"))
+            .withType("GameStarted")
             .withTime(Instant.now().atOffset(ZoneOffset.UTC))
-            .withSubject("subject")
             .withDataContentType("application/json")
             .withData("{ \"message\" : \"hello\" }".toByteArray())
             .build();
 
         val eventStore = InMemoryEventStore()
 
-        val eventStreamBefore: EventStream<CloudEvent> = eventStore.read("user1")
+        eventStore.write("game1", Stream.of(event))
 
-        eventStore.write(
-            "user1",
-            eventStreamBefore.version(),
-            Stream.of(event)
-        )
-
-        val eventStream: EventStream<CloudEvent> = eventStore.read("user1")
-        println(eventStream.events().findFirst())
-    }
-}
-
-
-@Nested
-class Domain {
-    @Test
-    fun correctGuess() {
-        val events = StartNewGameCommand("game1", "hello").decide(sequenceOf())
-        val result = GuessWordCommand("hello").decide(events)
-        assertThat(result.first()).isEqualTo(GuessedCorrectly("game1", "hello"))
-    }
-
-    @Test
-    fun wrongGuess() {
-        val events = StartNewGameCommand("game1", "hello").decide(sequenceOf())
-        val result = GuessWordCommand("xxx").decide(events)
-        assertThat(result.first()).isEqualTo(GuessedWrongly("game1", "xxx"))
-    }
-
-    @Test
-    fun gameAlreadyStarted() {
-        val events = sequenceOf(GameStarted("game1", "hello"))
-        assertThatThrownBy {
-            StartNewGameCommand("game1", "hello").decide(events)
+        val eventStream: EventStream<CloudEvent> = eventStore.read("game1")
+        eventStream.events().toList().map {
+            println(it)
+            println(String(it.data!!.toBytes()))
         }
     }
+
+    @Nested
+    inner class Domain {
+        @Test
+        fun correctGuess() {
+            val setUp = Stream.of<DomainEvent>(GameStarted("game1", "hello"))
+            val result = GuessWordCommand("hello").decide(setUp)
+            assertThat(result.findFirst().getOrNull()).isEqualTo(GuessedCorrectly("game1", "hello"))
+        }
+
+        @Test
+        fun wrongGuess() {
+            val setUp = Stream.of<DomainEvent>(GameStarted("game1", "hello"))
+            val result = GuessWordCommand("xxx").decide(setUp)
+            assertThat(result.findFirst().getOrNull()).isEqualTo(GuessedWrongly("game1", "xxx"))
+        }
+
+        @Test
+        fun gameAlreadyStarted() {
+            val setUp = Stream.of<DomainEvent>(GameStarted("game1", "hello"))
+            assertThatThrownBy {
+                StartNewGameCommand("game1", "hello").decide(setUp)
+            }
+        }
+    }
+
+    @Test
+    fun `manual lifecycle`() {
+        val eventStore = InMemoryEventStore()
+        val objectMapper = ObjectMapper().apply {
+            registerModule(KotlinModule.Builder().build())
+        }
+        val cloudEventConverter = JacksonCloudEventConverter<DomainEvent>(
+            objectMapper,
+            URI.create("com:fairtiq:guess_game")
+        )
+
+        eventStore.read("game1").let { oldStoredEvents ->
+            val oldDomainEvents = oldStoredEvents.events().map(cloudEventConverter::toDomainEvent)
+            val newDomainEvents = StartNewGameCommand("game1", "hello").decide(oldDomainEvents)
+            val newStoredEvents = newDomainEvents.map(cloudEventConverter::toCloudEvent)
+            eventStore.write("game1", newStoredEvents)
+        }
+//
+//        eventStore.read("game1").let { oldStoredEvents ->
+//            val oldDomainEvents = oldStoredEvents.events().map(cloudEventConverter::toDomainEvent)
+//            val newDomainEvents = GuessWordCommand("hello").decide(oldDomainEvents)
+//            val newStoredEvents = newDomainEvents.map(cloudEventConverter::toCloudEvent)
+//            eventStore.write("game1", newStoredEvents)
+//        }
+
+        eventStore.read("game1").toList().map(::println)
+    }
+
+    @Test
+    fun `wrapped into service`() {
+        val eventStore = InMemoryEventStore()
+        val objectMapper = ObjectMapper().apply {
+            registerModule(KotlinModule.Builder().build())
+        }
+        val cloudEventConverter = JacksonCloudEventConverter<DomainEvent>(
+            objectMapper,
+            URI.create("com:fairtiq:guess_game")
+        )
+
+        val applicationService = GenericApplicationService(eventStore, cloudEventConverter)
+
+        applicationService.execute("game1") { events ->
+            StartNewGameCommand("game1", "hello").decide(events)
+        }
+        applicationService.execute("game1") { events ->
+            GuessWordCommand("xxx").decide(events)
+        }
+
+        applicationService.execute("game1") { events ->
+            GuessWordCommand("hello").decide(events)
+        }
+
+        eventStore.read("game1").toList().map(::println)
+    }
+
+    /**
+     * TODO
+     * * MongoDB
+     * * explain concurrency
+     */
+
+    /**
+     * Not part of this 101
+     * * snapshots (closing books)
+     * * dynamic consistency boundary
+     */
 }
+
+
 
 
 
